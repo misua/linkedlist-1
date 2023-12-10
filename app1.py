cf40418fb8209739f8276d20c54311a081b56fb2
misua <charlespogzz@googlemail.com>
2023-12-10 19:29:35 +0800

    added set(index,value)

--- app1.py
+++ app1.py
@@ -6,12 +6,12 @@ class Node:
     def __init__(self,value=None):
         self.value = value
         self.next: Optional[Node] = None
-        #self.next : Node
+        self.prev = None
 
 
 #constructor
 class LinkedList:
-    def __init__(self,value):
+    def __init__(self,value=None):
        new_node = Node(value) # create a new node calling class Node
        self.head = new_node #make it point to 1st node created, same as tail
        self.tail = new_node
@@ -28,13 +28,45 @@ class LinkedList:
             self.tail = new_node # i guess its the same. tail  assigned to new_node
         self.length +=1
         return True
-
+    
+    # -----------------
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
+    # -----------------
+    def get(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+
+    # def getty(self,index):
+    #     if index < 0 or index >= self.length:
+    #         return None
+
+    #     if index > self.length // 2:
+    #         temp = self.tail
+    #         for _ in range(self.length - index -1):
+    #             if temp.prev is None:
+    #                 return None
+    #             temp = temp.prev
+    #     else:
+    #         temp = self.head
+    #         for _ in range(index):
+    #             if temp.next is None:
+    #                 return None
+    #             temp = temp.next
+
+    #         return temp   
+
+    # -----------------
   
     def pop_first(self,value): #remove first index from list
         if self.head is None: # this can self.length == 0 return None
@@ -61,6 +93,7 @@ class LinkedList:
     #         self.tail = None
     #     return temp
 
+    # -----------------
 
     def pop_last(self,value): #pop -  Last in first out
         if self.length == 0:
@@ -69,13 +102,13 @@ class LinkedList:
         pre = self.head
 
         #while (temp.next): is true
-        while temp.next is not None:
+        while temp.next is not None and temp.value != value:
         #and temp.value != value:
             pre = temp 
             temp = temp.next 
         
-        if temp is None:
-            return None
+        # if temp is None:
+        #     return None
         
         if pre is None:
             self.head = temp.next
@@ -104,39 +137,46 @@ class LinkedList:
             self.head = new_node #point the NEW head node to new_node
         self.length += 1
         return True
+    
+    def set_value1(self,index,value): #b3lat wa na mao!
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+        
 
-
-
+# ----------------------------------
 
 
 my_linked_list = LinkedList(1)
 print(my_linked_list.print_list())
+
 print("--running append func")
 my_linked_list.append(2)
-
-
-
 my_linked_list.print_list()
 
 
+# print("--running prepend(add item b4 index of List)")
+# my_linked_list.prepend(0)
+# my_linked_list.print_list()
 
-print("--running  prepend(add item before index of List)")
-my_linked_list.prepend(0)
+# print("--running remove(remove 1st index sa list)")
+# my_linked_list.pop_first(0)
+# my_linked_list.print_list()
 
+# print("--running pop() (ibta ang tail sa list)")
+# my_linked_list.pop_last(2)
+# my_linked_list.print_list()
+my_linked_list.append(5)
+my_linked_list.append(6)
+
+print("--running set value w/ loc as index val")
+my_linked_list.set_value1(1,66)
 my_linked_list.print_list()
+# input("press enter to clear sceen")
 
-print("--running remove(first index sa list)")
-
-
-my_linked_list.pop_first(0)
-my_linked_list.print_list()
-
-print("--running pop(ibta ang tail sa list)")
-my_linked_list.pop_last(2)
-my_linked_list.print_list()
-#input("press enter to clear sceen")
-
-#print('\033c', end='')
+# print('\033c', end='')
 #print(my_linked_list.pop1())
 #print(my_linked_list.pop1())
 #print(my_linked_list.pop1())
